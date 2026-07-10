2cb2b5e6c3eedfc15c69c675bd2994a8fd026724
heman chandra <hemanchandra701@gmail.com>
2026-07-10 22:22:19 +0530

    shell

---
+++ 08-special-var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Arguments passed to a script: $@"
+echo "Arguments passed to a script: $*"
+echo "Script name is: $0"
+echo "User of the script: $USER"
+echo "Present Dir of the script: $PWD"
+echo "User Dir of the script: $HOME"

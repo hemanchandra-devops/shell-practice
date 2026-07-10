7107ae7dc072d42ba083c5b69aa9e538a65a3dfe
heman chandra <hemanchandra701@gmail.com>
2026-07-10 22:08:12 +0530

    shell

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+echo "$Person1:: How are you?"
+echo "$Person2:: I am fine"
+echo "$Person1:: What are you learning?"
+echo "$Person2:: I am learning Aws DevOps"

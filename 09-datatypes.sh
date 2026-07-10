7fca2d9425e2fede8da72e7bcafe079897ea0173
heman chandra <hemanchandra701@gmail.com>
2026-07-10 22:31:39 +0530

    shell

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -6,3 +6,6 @@ echo "sum is $c"
 
 leaders=("kcr" "ktr" "revanth" "modi")
 echo "leaders are: ${leaders[@]}"
+echo "first leaders is: ${leaders[0]}"
+echo "last leaders is: ${leaders[3]}"
+echo "last leaders is: ${leaders[10]}"

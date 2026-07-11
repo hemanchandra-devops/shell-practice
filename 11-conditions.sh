a4293151e6fcd521bca946e3d74ccf6999b9bcbb
heman chandra <hemanchandra701@gmail.com>
2026-07-11 11:33:01 +0530

    shell

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,7 +1,7 @@
 echo "Enter a number: "
 read Number
 
-if [ $(($Number/2)) -ne 0 ];then
+if [ $(($Number%2)) -ne 0 ];then
     echo "Given $Number is Odd Number"
 else
     echo "Given $Number is Even Number"

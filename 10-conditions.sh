24a68e051ae4c6a8553f4f0814109c926dc3a0a9
heman chandra <hemanchandra701@gmail.com>
2026-07-11 11:30:55 +0530

    shell

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,7 +5,7 @@ Number=$1
 if [ $Number -lt 10 ];then
     echo "Given $Number is less than 10"
 elif [ $Number -eq 10 ];then
-    echo "Given $Number is euql to 10"
+    echo "Given $Number is equal to 10"
 else
     echo "Given $Number is greater than 10"
 fi
---
+++ 11-conditions.sh
@@ -0,0 +1,7 @@
+echo "Enter a number: "
+read Number
+
+if [ $(($Number / 2)) -ne 0 ];then
+    echo "Given $Number is Odd Number"
+else
+    echo "Given $Number is Even Number"

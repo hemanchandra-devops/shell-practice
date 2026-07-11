ff1453c96546d6d5783e1aaac983b4b3be810b3a
heman chandra <hemanchandra701@gmail.com>
2026-07-11 11:56:36 +0530

    shell

---
+++ 15-colors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m" 
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ];then
+    echo -e "$R Please run this script with Root Access! $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];then
+        echo -e "$R Installing $2 ... $N Failure"
+        exit 1
+    else 
+        echo -e "$G Installing $2 ... $N Success"
+    fi
+}
+
+dnf list available mysql
+if [ $? -ne 0 ];then
+    dnf install mysql -y
+    VALIDATE $? "MySql"
+else
+    echo "Already Installed Mysql"
+fi
+
+dnf list available nginx
+if [ $? -ne 0 ];then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "Already Installed Nginx"
+fi
+
+dnf list available redis
+if [ $? -ne 0 ];then
+    dnf install redis -y
+    VALIDATE $? "Redis"
+else
+    echo "Already Installed Redis"
+fi

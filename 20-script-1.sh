58fa59b8a88414b97364103e72e1ec342a50c2e4
heman chandra <hemanchandra701@gmail.com>
2026-07-13 12:26:46 +0530

    shell

---
+++ 20-script-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Name=Heman
+echo "Name is: $Name"
+echo "PID of the script-1 is: $$"
+
+sh 21-script-2.sh
+# source ./21-script-2.sh
---
+++ 21-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Name is: $Name"
+echo "PID of the script-2 is: $$"
